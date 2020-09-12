a00a2eaf218114e72a57cdd8e4bd119167e25518
JohannesKonings <johannes.konings@outlook.com>
2020-09-12 15:25:54 +0200

    fruit data

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const fruitBasket1 = [
+  { fruitName: "Apple", count: 5 },
+  { fruitName: "Banana", count: 3 },
+  { fruitName: "Strawberry", count: 9 },
+  { fruitName: "Lemon", count: 7 }
+];
+
+const fruitBasket2 = [
+  { fruitName: "Apple", count: 1 },
+  { fruitName: "Banana", count: 8 },
+  { fruitName: "Lemon", count: 3 }
+];
+
+const fruitBasket3 = [
+  { fruitName: "Apple", count: 2 },
+  { fruitName: "Banana", count: 3 },
+  { fruitName: "Orange", count: 3 },
+  { fruitName: "Lemon", count: 9 }
+];
