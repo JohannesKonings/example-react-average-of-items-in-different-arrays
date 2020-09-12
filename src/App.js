1f6085211d713d4802e5fff0d63fccb4ce894386
JohannesKonings <johannes.konings@outlook.com>
2020-09-12 16:34:14 +0200

    textfield, buttons, ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+
+import Button from "@material-ui/core/Button";
+
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+
+import { fruitBaskets as allFruitBaskets } from "./data";
+
+const useStyles = makeStyles(theme => ({
+  textField: {
+    width: "25ch",
+    margin: theme.spacing(1)
+  }
+}));
+
 export default function App() {
+  const classes = useStyles();
+
+  const [fruitBaskets, setFruitBaskets] = useState([
+    { fruitBasket: "" },
+    { fruitBasket: "" }
+  ]);
+
+  const handleTextFieldFruitBasketChange = (event, index) => {
+    const value = event.target.value;
+    const list = [...fruitBaskets];
+    list[index]["fruitBasket"] = value;
+    setFruitBaskets(list);
+  };
+
+  const handleAddClick = () => {
+    setFruitBaskets([...fruitBaskets, { fruitBasket: "" }]);
+  };
+
+  const handleRemoveClick = index => {
+    const list = [...fruitBaskets];
+    list.splice(index, 1);
+    setFruitBaskets(list);
+  };
+
+  const handleCalculateAverage = async () => {
+    console.log("handleCalculateAverage presed");
+  };
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {fruitBaskets.map((x, i) => {
+        return (
+          <div key={i} className="box">
+            {" "}
+            <TextField
+              id="fruitBaskteTextField"
+              className={classes.textField}
+              label="fruit basket"
+              select
+              value={x.fruitBasket}
+              onChange={e => handleTextFieldFruitBasketChange(e, i)}
+            >
+              {" "}
+              {allFruitBaskets.map(option => (
+                <MenuItem key={option.name} value={option.name}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </TextField>
+            {fruitBaskets.length !== 1 && (
+              <IconButton aria-label="remove" onClick={handleRemoveClick}>
+                <RemoveIcon fontSize="large" />
+              </IconButton>
+            )}
+            {fruitBaskets.length - 1 === i && (
+              <IconButton aria-label="add" onClick={handleAddClick}>
+                <AddIcon fontSize="large" />
+              </IconButton>
+            )}
+          </div>
+        );
+      })}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleCalculateAverage}
+      >
+        Calculate average
+      </Button>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,19 +1,25 @@
-const fruitBasket1 = [
+export const fruitBasket1 = [
   { fruitName: "Apple", count: 5 },
   { fruitName: "Banana", count: 3 },
   { fruitName: "Strawberry", count: 9 },
   { fruitName: "Lemon", count: 7 }
 ];
 
-const fruitBasket2 = [
+export const fruitBasket2 = [
   { fruitName: "Apple", count: 1 },
   { fruitName: "Banana", count: 8 },
   { fruitName: "Lemon", count: 3 }
 ];
 
-const fruitBasket3 = [
+export const fruitBasket3 = [
   { fruitName: "Apple", count: 2 },
   { fruitName: "Banana", count: 3 },
   { fruitName: "Orange", count: 3 },
   { fruitName: "Lemon", count: 9 }
+];
+
+export const fruitBaskets = [
+  { name: "fruitBasket1" },
+  { name: "fruitBasket2" },
+  { name: "fruitBasket3" }
 ];
